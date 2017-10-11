cabf0ae2b8afcda3dbad60d86bbe0c32daec58f8
Harkame <louisr.daviaud@gmail.com>
2017-10-11 12:15:03 +0200

    Fix

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,10 +1,30 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class Graphe
 {
+	private ArrayList<Summit> summits;
+	
+	public Graphe()
+	{
+		summits = new ArrayList<Summit>();
+	}
+	
+	public void colorate(Graphe grapheToColorate)
+	{
+		
+	}
+	
+	public void addSummit(Summit summitToAdd)
+	{
+		summits.add(summitToAdd);
+	}
+	
 	@Override
 	public String toString()
 	{
-		StringBuilder r_string_builder = new StringBuilder();
+		StringBuilder toString = new StringBuilder();
 		
-		return r_string_builder.toString();
+		return toString.toString();
 	}
 }
---
+++ src/Summit.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+class Summit
+	{
+		private String name;
+		private int color;
+		
+		private ArrayList<Summit> neighbors;
+		
+		public Summit(String name)
+		{
+			this.name = name;
+			color = 0;
+			
+			neighbors = new ArrayList<Summit>();
+		}
+		
+		public static Summit getColorableSummit(ArrayList<Summit> summits, int k)
+		{
+			
+		}
+		
+		public int getFirstAvailableColor()
+		{
+			int color = 1;
+			boolean colorAvailable = false;
+			
+			w:while(!colorAvailable)
+				for(Summit neighbor : neighbors)
+					if(neighbor.color == color)
+					{
+						color++;
+						break w;
+					}
+					else
+						colorAvailable = true;
+			
+			return color;
+		}
+		
+		public int getDegree()
+		{
+			return neighbors.size();
+		}
+		
+		public void link(Summit summitToLink)
+		{
+			this.neighbors.add(summitToLink);
+			
+			summitToLink.neighbors.add(this);
+		}
+		
+		@Override
+		public String toString()
+		{
+			StringBuilder toString = new StringBuilder();
+			
+			return toString.toString();
+		}
+	}
