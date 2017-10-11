4a634fb9b548173504a4e15dc2bf4e6d3376a794
Harkame <louisr.daviaud@gmail.com>
2017-10-11 11:49:19 +0200

    First

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Graphe
 {
@@ -7,5 +8,23 @@ public class Graphe
 		StringBuilder r_string_builder = new StringBuilder();
 		
 		return r_string_builder.toString();
+	}
+	
+	class Sommet
+	{
+		private ArrayList<Sommet> a_neighbors;
+		
+		public Sommet()
+		{
+			a_neighbors = new ArrayList<Sommet>();
+		}
+		
+		@Override
+		public String toString()
+		{
+			StringBuilder r_string_builder = new StringBuilder();
+			
+			return r_string_builder.toString();
+		}
 	}
 }
