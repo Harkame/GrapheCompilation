fcd9e4ed4fd9a32b2ad69d194a96b8d70428842f
Harkame <louisr.daviaud@gmail.com>
2017-10-11 11:56:01 +0200

    Fix

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class Graphe
 {
 	@Override
@@ -8,23 +6,5 @@ public class Graphe
 		StringBuilder r_string_builder = new StringBuilder();
 		
 		return r_string_builder.toString();
-	}
-	
-	class Sommet
-	{
-		private ArrayList<Sommet> a_neighbors;
-		
-		public Sommet()
-		{
-			a_neighbors = new ArrayList<Sommet>();
-		}
-		
-		@Override
-		public String toString()
-		{
-			StringBuilder r_string_builder = new StringBuilder();
-			
-			return r_string_builder.toString();
-		}
 	}
 }
---
+++ src/Sommet.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class Sommet
+	{
+		private ArrayList<Sommet> a_neighbors;
+		
+		public Sommet()
+		{
+			a_neighbors = new ArrayList<Sommet>();
+		}
+		
+		@Override
+		public String toString()
+		{
+			StringBuilder r_string_builder = new StringBuilder();
+			
+			return r_string_builder.toString();
+		}
+	}
