cb9970123f0055d360efc9b012826ae20ea79609
Harkame <louisr.daviaud@gmail.com>
2017-10-18 10:04:01 +0200

    Remove package, use System.lineSeparator()

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,88 @@
-package main;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+
+class Main {
+
+    private static void printHelp(){
+        System.out.println("Usage : java Main <k : coloration> <n : numero_de_graphe>");
+        System.out.println("Avec n = : ");
+        System.out.println("\t Graphe 1 : Losange");
+        System.out.println("\t Graphe 2 : Exemple du cours");
+        System.out.println("\t Graphe 3 : Binôme");
+        System.out.println("\t Graphe 4 : Monôme");
+    }
+
+    public static void main (String [] args)
+    {
+
+        if (args.length < 2){
+            printHelp();
+            return;
+        }
+
+
+        int k = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+        Graphe graphe = new Graphe();
+
+        switch (n){
+            case 1:
+                Summit a = new Summit("a");
+                Summit b = new Summit("b");
+                Summit c = new Summit("c");
+                Summit d = new Summit("d");
+
+                a.link(b);
+                b.link(c);
+                c.link(d);
+                d.link(a);
+
+                graphe.addSummits(a, b, c, d);
+
+                break;
+            case 2:
+                Summit t = new Summit("t");
+                Summit u = new Summit("u");
+                Summit v = new Summit("v");
+                Summit x = new Summit("x");
+                Summit y = new Summit("y");
+                Summit z = new Summit("z");
+
+                t.link(v);
+                t.link(y);
+                z.link(v);
+                v.link(x);
+                u.link(x);
+                y.link(x);
+                y.link(u);
+
+                graphe.addSummits(t, u, v, x, y, z);
+
+                break;
+            case 3:
+                Summit s = new Summit("a");
+                Summit r = new Summit("b");
+                s.link(r);
+                graphe.addSummits(r, s);
+
+                break;
+            case 4:
+                graphe.addSummits(new Summit("a"));
+                break;
+            default:
+                System.err.println("Le graphe demandé n'existe pas");
+                return;
+        }
+
+        graphe.colorate(k);
+        System.out.println("\n-----------------------\nGraphe final : \n" + graphe);
+
+    }
+
+}
 
 class Summit
 	{
@@ -92,7 +171,7 @@ class Summit
 		@Override
 		public String toString()
 		{
-		    String ls = System.getProperty("line.separator");
+		    String ls = System.lineSeparator();
 			StringBuilder toString = new StringBuilder();
 			toString.append(name).append(" : (couleur ").append(color).append(") -->");
 			for (Summit n : neighbors){
@@ -237,7 +316,7 @@ class Graphe
 	public String toString()
 	{
 
-	    String ls = System.getProperty("line.separator");
+	    String ls = System.lineSeparator();
 		StringBuilder toString = new StringBuilder();
 
 		toString.append("Sommets :");
@@ -258,85 +337,4 @@ class Graphe
 
 		return toString.toString();
 	}
-}
-
-public class Main {
-
-    private static void printHelp(){
-        System.out.println("Usage : java -jar monExe.jar <k : coloration> <n : numero_de_graphe>");
-        System.out.println("Avec n = : ");
-        System.out.println("\t Graphe 1 : Losange");
-        System.out.println("\t Graphe 2 : Exemple du cours");
-        System.out.println("\t Graphe 3 : Binôme");
-        System.out.println("\t Graphe 4 : Monôme");
-    }
-
-    public static void main (String [] args)
-    {
-
-        if (args.length < 2){
-            printHelp();
-            return;
-        }
-
-
-        int k = Integer.parseInt(args[0]);
-        int n = Integer.parseInt(args[1]);
-
-        Graphe graphe = new Graphe();
-
-        switch (n){
-            case 1:
-                Summit a = new Summit("a");
-                Summit b = new Summit("b");
-                Summit c = new Summit("c");
-                Summit d = new Summit("d");
-
-                a.link(b);
-                b.link(c);
-                c.link(d);
-                d.link(a);
-
-                graphe.addSummits(a, b, c, d);
-
-                break;
-            case 2:
-                Summit t = new Summit("t");
-                Summit u = new Summit("u");
-                Summit v = new Summit("v");
-                Summit x = new Summit("x");
-                Summit y = new Summit("y");
-                Summit z = new Summit("z");
-
-                t.link(v);
-                t.link(y);
-                z.link(v);
-                v.link(x);
-                u.link(x);
-                y.link(x);
-                y.link(u);
-
-                graphe.addSummits(t, u, v, x, y, z);
-
-                break;
-            case 3:
-                Summit s = new Summit("a");
-                Summit r = new Summit("b");
-                s.link(r);
-                graphe.addSummits(r, s);
-
-                break;
-            case 4:
-                graphe.addSummits(new Summit("a"));
-                break;
-            default:
-                System.err.println("Le graphe demandé n'existe pas");
-                return;
-        }
-
-        graphe.colorate(k);
-        System.out.println("\n-----------------------\nGraphe final : \n" + graphe);
-
-    }
-
 }
