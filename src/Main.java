9adc6a32896b793c87e3d53672073cfde758b4a9
tkriszt <theo@kriszt.fr>
2017-10-18 10:32:39 +0200

    adddPrefferedLink

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 class Main {
 
-    private static void printHelp(){
+    private static void printHelp() {
         System.out.println("Usage : java Main <k : coloration> <n : numero_de_graphe>");
         System.out.println("Avec n = : ");
         System.out.println("\t Graphe 1 : Losange");
@@ -18,10 +18,9 @@ class Main {
         System.out.println("\t Graphe 4 : Monôme");
     }
 
-    public static void main (String [] args)
-    {
+    public static void main(String[] args) {
 
-        if (args.length < 2){
+        if (args.length < 2) {
             printHelp();
             return;
         }
@@ -32,7 +31,7 @@ class Main {
 
         Graphe graphe = new Graphe();
 
-        switch (n){
+        switch (n) {
             case 1:
                 Summit a = new Summit("a");
                 Summit b = new Summit("b");
@@ -63,6 +62,8 @@ class Main {
                 y.link(x);
                 y.link(u);
 
+                u.prefferLink(t);
+
                 graphe.addSummits(t, u, v, x, y, z);
 
                 break;
@@ -87,20 +88,17 @@ class Main {
     }
 }
 
-class Graphe
-{
-	private List<Summit> summits;
-	private List<Summit> summitsSpilled;
+class Graphe {
+    private List<Summit> summits;
+    private List<Summit> summitsSpilled;
 
-	public Graphe()
-	{
-		summits = new ArrayList<>();
-		summitsSpilled = new ArrayList<>();
+    public Graphe() {
+        summits = new ArrayList<>();
+        summitsSpilled = new ArrayList<>();
     }
 
-	public void colorate(int k)
-	{
-		if(summits.size() == 0) {
+    public void colorate(int k) {
+        if (summits.size() == 0) {
             return;
         }
 
@@ -109,8 +107,8 @@ class Graphe
 
         Graphe newGraphe = this.copy();
 
-        if(summit != null) // un sommet est trivialement colorable
-		{
+        if (summit != null) // un sommet est trivialement colorable
+        {
             newGraphe.removeSummit(summit);
             newGraphe.colorate(k);
             raiseModifications(newGraphe);
@@ -119,59 +117,58 @@ class Graphe
             System.out.println("Coloration du sommet " + summit.getName() + " en couleur " + summit.getColor());
 
 
-		}
-		else
-		{
+        } else {
             summit = Summit.getHighestDegreeSummit(newGraphe.summits);
             System.out.println("Spilling du sommet " + summit.getName());
 
             newGraphe.removeSummit(summit);
-			newGraphe.colorate(k);
-			raiseModifications(newGraphe);
-			summitsSpilled.add(summit);
+            newGraphe.colorate(k);
+            raiseModifications(newGraphe);
+            summitsSpilled.add(summit);
         }
 
-	}
+    }
 
     /**
      * Remonte au graphe courant les modifications effectuées par la coloration récursive (car copie en profondeur, non par référence)
+     *
      * @param newGraphe le graphe coloré par récursion
      */
     private void raiseModifications(Graphe newGraphe) {
-	    for (Summit n : newGraphe.summits){
-	        for (Summit s : summits){
-	            if (n.equals(s)){
-	                s.setColor(n.getColor());
+        for (Summit n : newGraphe.summits) {
+            for (Summit s : summits) {
+                if (n.equals(s)) {
+                    s.setColor(n.getColor());
                 }
             }
         }
 
-        for (Summit s : newGraphe.summitsSpilled){
-	        summitsSpilled.add(s);
+        for (Summit s : newGraphe.summitsSpilled) {
+            summitsSpilled.add(s);
         }
     }
 
-    private Graphe copy(){
-	    Graphe graphe = new Graphe();
+    private Graphe copy() {
+        Graphe graphe = new Graphe();
         HashMap<Summit, List<String>> links = new HashMap<>(); // stocker les liens de chaque sommet copié par noms
 
-	    for (Summit s : summits){
-	        Summit newSummit = s.copy();
+        for (Summit s : summits) {
+            Summit newSummit = s.copy();
             graphe.summits.add(newSummit);
 
             List<String> neighborsNames = new ArrayList<>();
-            for (Summit n : s.getNeighbors()){
+            for (Summit n : s.getNeighbors()) {
                 neighborsNames.add(n.getName());
             }
             links.put(newSummit, neighborsNames);
         }
 
 
-	    for (Summit s : summitsSpilled)
-	        graphe.summitsSpilled.add(s.copy());
+        for (Summit s : summitsSpilled)
+            graphe.summitsSpilled.add(s.copy());
 
-	    //Après ajout des sommets, réappliquer les liens sur les nouveaux sommets
-	    for (Summit s : graphe.summits)
+        //Après ajout des sommets, réappliquer les liens sur les nouveaux sommets
+        for (Summit s : graphe.summits)
             for (String name : links.get(s))
                 for (Summit n : graphe.summits)
                     if (n.getName().equals(name))
@@ -180,185 +177,175 @@ class Graphe
         return graphe;
     }
 
-	public void addSummits(Summit... summitsToAdd)
-	{
+    public void addSummits(Summit... summitsToAdd) {
         Collections.addAll(summits, summitsToAdd);
-	}
+    }
 
-	private void removeSummit(Summit summitToRemove)
-	{
-        for (Summit neighbor : summits){
+    private void removeSummit(Summit summitToRemove) {
+        for (Summit neighbor : summits) {
             neighbor.removeNeighbor(summitToRemove);
         }
-		summits.remove(summitToRemove);
-	}
+        summits.remove(summitToRemove);
+    }
 
-	@Override
-	public String toString()
-	{
+    @Override
+    public String toString() {
 
-	    String ls = System.lineSeparator();
-		StringBuilder toString = new StringBuilder();
+        String ls = System.lineSeparator();
+        StringBuilder toString = new StringBuilder();
 
-		toString.append("Sommets :");
-		toString.append(ls);
+        toString.append("Sommets :");
+        toString.append(ls);
 
-		for(Summit summit : summits)
-			toString.append("\t").append(summit.toString());
+        for (Summit summit : summits)
+            toString.append("\t").append(summit.toString());
 
-		toString.append(ls);
-		toString.append("Pile :");
-		toString.append(ls);
+        toString.append(ls);
+        toString.append("Pile :");
+        toString.append(ls);
 
-		for(Summit summit : summitsSpilled) {
+        for (Summit summit : summitsSpilled) {
             toString.append("\t");
             toString.append(summit.getName());
             toString.append(ls);
         }
 
-		return toString.toString();
-	}
+        return toString.toString();
+    }
 }
 
-class Summit
-	{
-		private String name;
-		private int color;
+class Summit {
+    private String name;
+    private int color;
 
-		private List<Summit> neighbors;
-		private List<Summit> preferences;
+    private List<Summit> neighbors;
+    private List<Summit> preferences;
 
-		public Summit(String name)
-		{
-			this.name = name;
-			color = 0;
+    public Summit(String name) {
+        this.name = name;
+        color = 0;
 
-			neighbors = new ArrayList<Summit>();
-			preferences = new ArrayList<Summit>();
-		}
-
-        public String getName() {
-            return name;
-        }
-
-        public static Summit getColorableSummit(List<Summit> summits, int k)
-		{
-
-			for(Summit summit : summits){
-                if (summit.getDegree() < k && summit.color == 0){
-                    return summit;
-                }
-            }
-			return null;
-		}
-
-		public static Summit getHighestDegreeSummit(List<Summit> summits){
-			Summit summit = summits.get(0);
-
-			for (Summit s : summits){
-				if (summit.getDegree() < s.getDegree())
-					summit = s;
-			}
-			return summit;
-		}
-
-		public int getFirstAvailableColor()
-		{
-            int color = 1;
-			boolean colorAvailable = false;
-
-			if (neighbors.size() == 0) {
-			    colorAvailable = true;
-            }
-
-			while(!colorAvailable)
-			{
-				for(int index = 0; index < neighbors.size(); index++)
-				{
-					if(neighbors.get(index).color == color)
-					{
-						color++;
-						break;
-					}
-
-					if(index == neighbors.size() - 1)
-						colorAvailable = true;
-				}
-			}
-
-            return color;
-		}
-
-		private int getDegree()
-		{
-			return neighbors.size();
-		}
-
-		public void link(Summit summitToLink)
-		{
-			this.neighbors.add(summitToLink);
-
-			summitToLink.neighbors.add(this);
-		}
-
-		public void setColor(int newColor)
-		{
-			color = newColor;
-		}
-
-		@Override
-		public String toString()
-		{
-		    String ls = System.lineSeparator();
-			StringBuilder toString = new StringBuilder();
-				
-			toString.append(name).append(" : (");
-			
-			if(color == 0)
-				toString.append("Spilled");
-			else
-				toString.append("Color : ").append(color);
-			
-			toString.append(") -->");
-			
-			for (Summit n : neighbors){
-				toString.append(ls).append("\t\t");
-				
-				
-				toString.append(n.name).append(" (");
-				
-				if(n.color == 0)
-					toString.append("Spilled");
-				else
-					toString.append("Color : ").append(n.color);
-				
-                toString.append(")");
-            }
-
-			toString.append(ls);
-
-			return toString.toString();
-		}
-
-		public void removeNeighbor(Summit summitToRemove) {
-			neighbors.remove( summitToRemove );
-		}
-
-		public Summit copy() {
-            return new Summit(this.name);
-		}
-
-        public List<Summit> getNeighbors() {
-            return neighbors;
-        }
-
-        @Override
-        public boolean equals(Object o){
-		    return o instanceof Summit && ((Summit) o).getName().equals(this.getName());
-        }
-
-        public int getColor() {
-            return color;
-        }
+        neighbors = new ArrayList<Summit>();
+        preferences = new ArrayList<Summit>();
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Summit getColorableSummit(List<Summit> summits, int k) {
+
+        for (Summit summit : summits) {
+            if (summit.getDegree() < k && summit.color == 0) {
+                return summit;
+            }
+        }
+        return null;
+    }
+
+    public static Summit getHighestDegreeSummit(List<Summit> summits) {
+        Summit summit = summits.get(0);
+
+        for (Summit s : summits) {
+            if (summit.getDegree() < s.getDegree())
+                summit = s;
+        }
+        return summit;
+    }
+
+    public int getFirstAvailableColor() {
+        int color = 1;
+        boolean colorAvailable = false;
+
+        if (neighbors.size() == 0) {
+            colorAvailable = true;
+        }
+
+        while (!colorAvailable) {
+            for (int index = 0; index < neighbors.size(); index++) {
+                if (neighbors.get(index).color == color) {
+                    color++;
+                    break;
+                }
+
+                if (index == neighbors.size() - 1)
+                    colorAvailable = true;
+            }
+        }
+
+        return color;
+    }
+
+    private int getDegree() {
+        return neighbors.size();
+    }
+
+    public void link(Summit summitToLink) {
+        this.neighbors.add(summitToLink);
+
+        summitToLink.neighbors.add(this);
+    }
+
+    public void setColor(int newColor) {
+        color = newColor;
+    }
+
+    @Override
+    public String toString() {
+        String ls = System.lineSeparator();
+        StringBuilder toString = new StringBuilder();
+
+        toString.append(name).append(" : (");
+
+        if (color == 0)
+            toString.append("Spilled");
+        else
+            toString.append("Color : ").append(color);
+
+        toString.append(") -->");
+
+        for (Summit n : neighbors) {
+            toString.append(ls).append("\t\t");
+
+
+            toString.append(n.name).append(" (");
+
+            if (n.color == 0)
+                toString.append("Spilled");
+            else
+                toString.append("Color : ").append(n.color);
+
+            toString.append(")");
+        }
+
+        toString.append(ls);
+
+        return toString.toString();
+    }
+
+    public void removeNeighbor(Summit summitToRemove) {
+        neighbors.remove(summitToRemove);
+    }
+
+    public Summit copy() {
+        return new Summit(this.name);
+    }
+
+    public List<Summit> getNeighbors() {
+        return neighbors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return o instanceof Summit && ((Summit) o).getName().equals(this.getName());
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void prefferLink(Summit t) {
+        this.preferences.add(t);
+    }
+
 }
